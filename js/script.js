e908a3957def814571c29bce27bea8f6d0b59572
Mary Xu <changx964@gatech.edu>
2020-12-08 18:24:13 -0500

    finished game creation section

--- js/script.js
+++ js/script.js
@@ -10,9 +10,33 @@ g1_close.onclick = function() {
 }
 
 // g2: lumberjack jack
+var g2 = document.getElementById("g2");
+var g2_modal = document.getElementById("g2-modal");
+var g2_close = document.getElementById("g2-close");
+g2.onclick = function() {
+  g2_modal.style.display = "block";
+}
+g2_close.onclick = function() {
+  g2_modal.style.display = "none";
+}
+
+// g3: Escape to Elysium
+var g3 = document.getElementById("g3");
+var g3_modal = document.getElementById("g3-modal");
+var g3_close = document.getElementById("g3-close");
+g3.onclick = function() {
+  g3_modal.style.display = "block";
+}
+g3_close.onclick = function() {
+  g3_modal.style.display = "none";
+}
 
 window.onclick = function(event) {
   if (event.target == g1_modal) {
     g1_modal.style.display = "none";
+  } else if (event.target == g2_modal) {
+    g2_modal.style.display = "none";
+  } else if (event.target == g3_modal) {
+    g3_modal.style.display = "none";
   }
 }
